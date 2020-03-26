1fe1750d1f0f79c828b889998bd1ff15c1501641
Mindaugas <mindaugas@Mindaugass-MacBook-Pro.local>
2020-03-26 19:12:05 +0200

    deploy backend on docker and create mongodb

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from './middleware/cors';
 import errorHandler from './middleware/error-handler';
 import routes from './routes';
@@ -12,8 +13,15 @@ app.use(cors);
 app.use(errorHandler);
 app.use('/api', routes);
 
-const server = http.createServer(app);
+mongoose
+    .connect(`mongodb://${process.env.MONGO_DB_ADDR}:${process.env.MONGO_DB_PORT}/project`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port', (server.address() as any).port);
-});
+        server.listen(process.env.PORT || 3000, () => {
+            console.log('Server is running on port', (server.address() as any).port);
+        });
+    })
+    .catch((error: Error) => {
+        throw error;
+    });
